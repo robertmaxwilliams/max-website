828126107ca9db585fb7d835b6a969aa0532cc6d
Robert Max Williams <robertmaxwilliams@gmail.com>
2018-10-30 12:07:41 -0400

    added TESTING magic file functionality and auto base dir to runner.lisp

--- runner.lisp
+++ runner.lisp
@@ -1,10 +1,13 @@
 ;; not part of the package, just convenience to start the server
 (format t "Welcome to max website dot website server have a good stay~%")
-;;(asdf:clear-configuration)
-;;(asdf:clear-system :max-website)
-;;(ql:uninstall :max-website)
 
-(pushnew (truename "/home/public/max-website/") ql:*local-project-directories*)
+;; if the TESTING magic file is in pwd, then set to my laptop's dir. Otherwise set to 
+;; server's dir. Not the best way but it works.
+(if (probe-file "TESTING")
+  (defparameter *max-website-dir* "/Users/max/Repos/website/")
+  (defparameter *max-website-dir* "/home/public/max-website/"))
+
+(pushnew (truename *max-website-dir*) ql:*local-project-directories*)
 (ql:register-local-projects)
 (ql:quickload :max-website)
-(max-website:start-website "/home/public/max-website/")
+(max-website:start-website *max-website-dir*)
