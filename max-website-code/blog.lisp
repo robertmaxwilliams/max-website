585cd32a410b01f0beb4c5971e156bcffd3266f2
Robert Max Williams <robertmaxwilliams@gmail.com>
2019-02-26 11:17:41 -0500

    made blog able to use html files

--- max-website-code/blog.lisp
+++ max-website-code/blog.lisp
@@ -1,9 +1,19 @@
 (in-package :max-website)
 
 ;; returns list of (pathname title)
+
+(defun find-html-or-md (blog-name)
+  (flet ((uri->path (ending) (merge-pathnames *blog-dir* (pathname (str:concat blog-name ending)))))
+    (let ((md-file (uri->path ".md"))
+          (html-file (uri->path ".html")))
+      (cond
+        ((probe-file md-file) md-file)
+        ((probe-file html-file) html-file)
+        (t (error "file not found for this url"))))))
+
 (defun blog-pathname-title (uri)
   (let* ((blog-name (car (last (str:split "/" uri))))
-	(file-pathname (merge-pathnames *blog-dir* (pathname (str:concat blog-name ".md")))))
+         (file-pathname (find-html-or-md blog-name)))
     (list file-pathname 
 	  (car (blog-title-and-preview file-pathname)))))
 
@@ -11,7 +21,11 @@
   (with-open-file (stream pathname)
     (progn
       (unused (take-n-lines stream 4))
-      (second-value (markdown (stream-get-contents stream) :stream nil)))))
+      (cond 
+        ((str:ends-with-p ".md" (namestring pathname))
+         (second-value (markdown (stream-get-contents stream) :stream nil)))
+        ((str:ends-with-p ".html" (namestring pathname))
+         (stream-get-contents stream))))))
 
 (print "ooooooooooooooOOOOOoooooooooooooooooooooooOOO")
 ; takes in filepath, returns (title preview)
@@ -28,9 +42,13 @@
 (defun blog-files ()
   (sort (remove-if #'null (mapcar #'(lambda (pathname)
 				(let ((namestring (file-namestring pathname)))
-				  (if (str:ends-with-p ".md" namestring)
-				      (append (list namestring (str:substring 0 -3 namestring))
-					    (blog-title-and-preview pathname)))))
+				  (cond 
+                    ((str:ends-with-p ".md" namestring)
+                     (append (list namestring (str:substring 0 -3 namestring))
+                             (blog-title-and-preview pathname)))
+                    ((str:ends-with-p ".html" namestring)
+                     (append (list namestring (str:substring 0 -5 namestring))
+                             (blog-title-and-preview pathname))))))
 				  (uiop:directory-files *blog-dir*)))
 	#'(lambda (a b) (blog-name-sort-by-date (cadr a) (cadr b)))))
 
@@ -44,9 +62,17 @@
 (defun blog-name-date-extractor (namestring)
   (mapcar #'parse-integer (subseq (str:split "-" namestring) 0 3)))
 
+(defun remove-tags (str)
+  (ppcre:regex-replace-all "\<[^>]*\>" str ""))
+
+  
+
 (defun generate-preview (stream)
   "takes in markdown string and converts to html preview"
-  (str:concat (str:trim (str:substring 0 200 (second-value (markdown (str:join " " (take-n-lines stream 5)) :stream nil)))) "..."))
+  (remove-tags 
+    (str:concat 
+      (str:trim 
+        (str:substring 0 200 (second-value (markdown (str:join " " (take-n-lines stream 5)) :stream nil)))) "...")))
 
 (defun less-than-int-list (a b)
   (cond ((null a) nil) ;; for when different length
