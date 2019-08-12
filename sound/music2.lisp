9719198fb5012b56ed0a6028cea3f932650f4fce
Robert Max Williams <robertmaxwilliams@gmail.com>
2019-08-12 11:41:21 -0700

    yummy sounds

--- sound/music2.lisp
+++ sound/music2.lisp
@@ -51,20 +51,11 @@
 
 (defparameter *sps* 44100)
 
-
-
-
-
-
-
-
-
-
 ;; ==============
 ;; INSTRUMENTS
 ;; ==============
 
-(defun saw (freq &optional (duration 1))
+(defun saw (freq &optional (duration 1) (amp 1))
   (let* ((width 1.0)
 	 (v (* 0.5 width (/ (* 440.0 freq) *sps*)))
 	 (x 0.0)
@@ -75,15 +66,25 @@
       (if (> x width)
 	  (setf x (- width)))
       (if (< time (* duration *sps*))
-	  (* 0.2 x)
+	  (* 0.2 x amp)
 	  :done))))
 
 (defun wobble-saw (freq1 freq2))
 
-(defun triangle (freq &optional (duration 1))
+(defun f-upramp (x)
+    (1- (* (/ (+ 1 (exp (- (/ x 100))))) 2)))
+
+(defun fade-in-fade-out (duration)
+  (lambda (time)
+    (if (>= time duration)
+	0
+	(* (f-upramp time) (f-upramp (- duration time))))))
+
+(defun triangle (freq &optional (duration 1) (amp 1))
   (let* ((width 1.0)
 	 (v (* width (/ (* 440.0 freq) *sps*)))
 	 (x 0.0)
+	 (fade (fade-in-fade-out (* *sps* duration)))
 	 (time 0))
     (lambda ()
       (incf time)
@@ -91,23 +92,22 @@
       (if (or (<= x (- width)) (>= x width))
 	  (setf v (- v)))
       (if (< time (* duration *sps*))
-	  x
+	  (* (f fade time) amp x)
 	  :done))))
 
 (defun random-logit ()
   (- (/ (random 256) 128.0) 1))
 (random-logit)
 
-(defun crash (slowness &optional (duration 1))
-  (let ((intensity 1.3)
-	(damping (- 1.0 (/ (expt 2 (+ 10 slowness)))))
+(defun crash (slowness &optional (duration 1) (intensity 1))
+  (let ((damping (- 1.0 (/ (expt 2 (+ 10 slowness)))))
 	(time 0))
     (lambda ()
       (incf time)
       (setq intensity (* intensity damping))
       (if (< time (* duration *sps*))
-	(* (random-logit) intensity)
-	:done))))
+	  (* (random-logit) intensity)
+	  :done))))
 
 
 (defun ramp-down (speed)
@@ -122,6 +122,7 @@
   (let ((ramp-downer (ramp-down speed)))
     (lambda ()
       (- 1 (f ramp-downer)))))
+
 (defvar foo (ramp-up 4))
 (f foo)
 (defun tink (freq &optional (duration 1))
@@ -163,6 +164,14 @@
 	  x
 	  :done))))
 
+(defun silence (duration)
+  (let ((time 0))
+    (lambda ()
+      (incf time)
+      (if (< time (* duration *sps*))
+	  0.0
+	  :done))))
+
 (let ((foo (tink 9 5)))
   (trivial-with-audio (:duration 4)
     (if (zerop *channel) (f foo) 0)))
@@ -191,8 +200,17 @@
 	   (single-list-unpacking-mapcar fun (cdr ls))))
     (t (cons (apply fun (car ls)) (single-list-unpacking-mapcar fun (cdr ls))))))
 
+(member 3 '(1 2 3))
+(defun special-lambdize-for-keywords (fun)
+  (lambda (&rest args)
+    (case (car args)
+      ('rest (apply #'silence (cdr args)))
+      (otherwise (apply fun args)))))
+
 (defun lambdize-track (track)
-    (single-list-unpacking-mapcar (symbol-function (car track)) (arg-preprocess (cdr track))))
+  (single-list-unpacking-mapcar
+   (special-lambdize-for-keywords (symbol-function (car track)))
+   (arg-preprocess (cdr track))))
 
 ;; ==============
 ;; CHONKATION
@@ -217,29 +235,24 @@
 
 (defvar foo (lambdize-track '(triangle 1 2 1 3/2)))
 
-(defun play-tracks (tracks)
+(defun play-tracks (duration tracks)
   (let ((chonks (mapcar #'lambdize-track tracks))
 	(sample 0))
-    (trivial-with-audio (:duration 30)
+    (trivial-with-audio (:duration duration)
       (if (= 0 *channel)
 	  (let ((sample-chonks (extract-step chonks)))
 	    (setq chonks (cadr sample-chonks))
 	    (setq sample (car sample-chonks))
 	    sample)
 	  sample))))
-(ignore 5)
-(defun pass-1 (x) x)
-(
-(destructuring-bind (nil a b) '(1 2 3)
-  (list a b))
-		    
+ 
 
-;; ==============
-;; PLAY SONG
-;; ==============
+ ;; ==============
+ ;; PLAY SONG
+ ;; ==============
 
-(play-tracks 
-          ;;  |   |   |   |   |   |   |   |   |   |   |   |   |
+ (play-tracks 
+  ;;  |   |   |   |   |   |   |   |   |   |   |   |   |
   '((triangle 1   3/2 5/2 3/2 1   2/3 (1 3))
     (triangle 1   2   1   2/3 6/5 1   (1 3))
     (crash    (:repeat 4 (4 2)))
@@ -249,19 +262,52 @@
     (saw      1   2.1 1   6/7 6/7 1   (1 3) (9/8 0.5) (8/9 0.5)
      (10/7 0.25) (12/7 0.25) (7/9 0.5) (9/8 0.2) (8/9 0.8) 1 (2 0.125) (4 0.125) 1)
     ))
+ (defun repeat (times x)
+   (loop repeat times collect x))
+
 (play-tracks
- '((crash    (:repeat 4 (3 2)))
-   (saw      (9/8 0.5) (8/9 0.5)
-     (10/7 0.25) (12/7 0.25) (7/9 0.5) (9/8 0.2) (8/9 0.8) 1 (2 0.125) (3 0.125) (2 0.5) 1
-     (10/7 0.25) (12/7 0.25) (7/9 0.5) (9/8 0.2) (8/9 0.8) 1 (2 0.125) (3 0.125) (2 0.5) 1)
-    (triangle (8/9 0.5) (16/81 0.5)
-     (10/7 0.25) (12/7 0.25) (7/9 0.5) (9/8 0.2) (8/9 0.8) 1 (2 0.125) (3 0.125) (2 0.3) 1
-     (10/7 0.25) (12/7 0.25) (7/9 0.5) (9/8 0.2) (8/9 0.8) 1 (4 0.125) (-2 0.125) (2 0.5) 1)
+ 15
+ `(
+   (tink  ,@(repeat 4 '(13 1)))
+   (tink (rest 0.05) ,@(repeat 4 '(8 1)))
+   (tink (rest 0.10) ,@(repeat 4 '(7.2 1)))
+   (crash (rest 4) ,@(repeat 4 `(3 2)))
+   (saw      (rest 4) (9/8 0.5) (8/9 0.5)
+	     (10/7 0.25) (12/7 0.25) (7/9 0.5) (9/8 0.2) (8/9 0.8) 1 (2 0.125) (3 0.125) (2 0.5) 1
+	     (10/7 0.25) (12/7 0.25) (7/12 0.5) (9/8 0.2) (8/9 0.8) 1 (2 0.125) (3 0.125) (2 0.5) 1)
+   (triangle (rest 4) (8/9 0.5) (16/81 0.5)
+	     (10/7 0.25) (12/7 0.25) (7/9 0.5) (9/8 0.2) (8/9 0.8) 1 (2 0.125) (3 0.125) (2 0.3) 1
+	     (10/7 0.25) (12/7 0.25) (7/9 0.5 3) (1 0.2 4) (8/9 0.8 3) 1 (4 0.125) (-2 0.125) (2 0.5) 1)
    ))
 
+(define-symbol-macro r '(rest 1))
+(list +r)
+(print '(1 2 +r))
+(play-tracks
+ 19
+ `(
+   (saw (rest 4)              (6 3 0.1)                         (3/2 2 0.2) (5/2 1 0.2) (5/3 1 0.2))
+   (triangle 1   2   1   2    (6 1 0.2) (3 1 0.1) (13/9 1 0.2)  ,r  (1 3)                             )
+   (triangle ,r  3/2 5/3 (3/2 2)        (4/5 2)                 ,r  ,r          3/2         3/2)
+   (triangle ,r ,r   3   1    (5/2 2)             5/3           (rest 4)                          )
+   ))
 
 (play-tracks
- '((tink (9 2) (9 2) (9 2))))
+ 10
+ `(
+   (tink ,@(repeat 4 '(9 0.2)))
+   (triangle  5/3)
+   (triangle  1)
+   (triangle  3/2)
+   ))
+
+(play-tracks
+ 2
+ `(
+   (tink ,@(repeat 4 '(9 1)))
+   (tink (rest 0.05) ,@(repeat 4 '(8 1)))
+   (tink (rest 0.10) ,@(repeat 4 '(7.2 1)))
+   ))
 
 (defparameter *kill* t)
 (defparameter *kill* nil)
